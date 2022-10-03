fe0ef8812f069363f6e94ceb750a8f8ab5e66ce7
Carlos <karucupcake@gmail.com>
2022-10-03 19:51:30 +0200

    finalizado

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var op1, op2, signo,resultado;
+op1 = parseInt(prompt("Introduce el primer operando"));
+op2 = parseInt(prompt("Introduce el segundo operando"));
+signo = prompt("Introduce el signo aritmético");
+switch (signo) {
+    case "+":
+        resultado=op1+op2;
+        break;
+    case "-":
+        resultado=op1-op2;
+        break;
+    case "*":
+        resultado=op1*op2;
+        break;
+    case "/":
+        resultado=op1/op2;
+        break;
+    case "%":
+        resultado=op1%op2;
+        break;
+
+    case "^":
+        resultado=op1**op2;
+        break;
+        default:
+            console.log("Uno de los operandos no es un número o no has introducido un simbolo aritmético");
+            break;
+}
+console.log(resultado);
